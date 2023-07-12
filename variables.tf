46c6a12e1e1ae797fa14277d52d0dd68be48e760
JackR42 <j.roumimper@gmail.com>
2023-07-12 17:45:14 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,23 @@
 # Variables.tf - Generic variables
 
+variable "sub-name" {
+    description = "The prefix defined for the Subscription"
+    default = "DEV"
+}
 variable "env-name" {
     description = "The name of the environment"
     default = "DEV"
 }
-variable "rg-name" {
-    description = "The name of the resource group"
-    default = "rg-default"
+variable "app-name" {
+    description = "The name of the application"
+    default = "Project51"
 }
 variable "loc-name" {
   description = "The name of the Azure Region in which all resources should be created."
   default = "westeurope"
 }
+variable "rg-name" {
+    description = "The name of the resource group"
+    default = "$(sub-name)-RG-$(app-name)-$(env)"
+}
+
