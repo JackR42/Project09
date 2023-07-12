345fbbeccea3688b349741891e75854ae48e7334
JackR42 <j.roumimper@gmail.com>
2023-07-12 15:31:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+### BEGIN INIT
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {}
+}
+
 ### BEGIN MAIN
 resource "azurerm_resource_group" "project" {
   name = "S2-RG-Project09"
