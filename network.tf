2451643ee126f026be4adf7602e62b9c531a8d42
JackR42 <j.roumimper@gmail.com>
2023-07-13 11:45:34 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,11 +4,15 @@
 resource "azurerm_virtual_network" "network-vnet" {
 #  name                = "${lower(replace(var.app_name," ","-"))}-${var.environment}-vnet"
   name                = "Project09-vnet-DEV"
-  address_space       = [var.network-vnet-cidr]
+#  address_space       = [var.network-vnet-cidr]
+  address_space       = "10.128.0.0/16"
+
 #  resource_group_name = azurerm_resource_group.network-rg.name
-   resource_group_name = azurerm_resource_group.project.name
+#   resource_group_name = azurerm_resource_group.project.name
+   resource_group_name = "S2-RG-Project09-DEV"
 #  location            = azurerm_resource_group.network-rg.location
-  location             = azurerm_resource_group.project.location
+#  location             = azurerm_resource_group.project.location
+  location             = "westeurope"
 
 tags = {
     application = var.app-name
