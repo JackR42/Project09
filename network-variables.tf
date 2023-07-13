79ec67397c5838541c980862cd5d3d9a72551d4b
JackR42 <j.roumimper@gmail.com>
2023-07-13 11:09:23 +0200

    Create network-variables.tf

---
+++ network-variables.tf
@@ -0,0 +1,11 @@
+# network-variables.tf
+
+############################
+## Core Network - Variables ##
+##############################
+
+variable "network-vnet-cidr" {
+  type        = string
+  description = "The CIDR of the network VNET"
+}
+
