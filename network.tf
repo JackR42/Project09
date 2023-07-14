de0a188c0a85cf7f967932072a707412e75ddad1
JackR42 <j.roumimper@gmail.com>
2023-07-14 13:00:35 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-# Create the VNET
+# Create the VNET - main network
 resource "azurerm_virtual_network" "project-vnet" {
   name                = "${var.app-name}-vnet-${var.env-name}"
   address_space       = [var.vnet-cidr]
@@ -11,3 +11,17 @@ resource "azurerm_virtual_network" "project-vnet" {
   }
 }
 
+# Create subnet Bastion
+resource "azurerm_subnet" "project-subnet-bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.project.name
+  virtual_network_name = azurerm_virtual_network.project-vnet.name
+  address_prefixes     = [var.subnet-cidr-bastion]
+}
+
+resource "azurerm_subnet" "project_subnet-vm1" {
+  name                = "${var.app-name}-subnet-vm1-${var.env-name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet-cidr-vm1]
+}
