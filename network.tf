b95866c308d0801e80da9e10e0322768b5d52031
JackR42 <j.roumimper@gmail.com>
2023-07-14 02:06:16 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # Create the VNET
 resource "azurerm_virtual_network" "project-vnet" {
-  name                = "${var.prefix}-${var.env-name}-vnet"
+  name                = "${var.app-name}-vnet-${var.env-name}"
   address_space       = [var.vnet-cidr]
   resource_group_name = azurerm_resource_group.project.name
   location            = azurerm_resource_group.project.location
