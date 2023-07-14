44796e45d3a3fae2b40c500030094be8664608cc
JackR42 <j.roumimper@gmail.com>
2023-07-14 14:08:50 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -25,3 +25,52 @@ resource "azurerm_subnet" "project-subnet-vm1" {
   virtual_network_name = azurerm_virtual_network.project-vnet.name
   address_prefixes     = [var.subnet-cidr-vm1]
 }
+
+### xxx
+
+# Create Network Security Group for VM Subnet and the corresponding rule for RDP from Azure Bastion
+resource "azurerm_network_security_group" "project-nsg-subnet-vm1" {
+  name                = "${var.app-name}-nsg-subnet-vm1-${var.env-name}"
+  resource_group_name = azurerm_resource_group.project.name
+  location            = azurerm_resource_group.project.location
+
+tags = {
+    subscription = var.sub-name
+    application = var.app-name
+    environment = var.env-name
+  }
+}
+
+resource "azurerm_network_security_rule" "inbound_allow_rdp" {
+  network_security_group_name = azurerm_network_security_group.project-nsg-subnet-vm1.name
+  name                = "Inbound_Allow_Bastion_RDP"
+  resource_group_name = azurerm_resource_group.project.name
+  location            = azurerm_resource_group.project.location
+  priority                    = 500
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = azurerm_subnet.project-subnet-bastion.address_prefixes[0]
+  destination_address_prefix  = azurerm_subnet.project-subnet-vm1.address_prefixes[0]
+}
+
+resource "azurerm_network_security_rule" "outbound_allow_subnet" {
+  network_security_group_name = azurerm_network_security_group.project-nsg-subnet-vm1.name
+  resource_group_name         = azurerm_resource_group.project.name
+  name                        = "Outbound_Allow_Subnet_Any"
+  priority                    = 500
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = azurerm_subnet.project-subnet-vm1.address_prefixes[0]
+  destination_address_prefix  = azurerm_subnet.project-subnet-vm1.address_prefixes[0]
+}
+
+resource "azurerm_subnet_network_security_group_association" "project-nsg-associate_subnet_vm1" {
+  network_security_group_id = azurerm_network_security_group.project-nsg-subnet-vm1.id
+  subnet_id                 = azurerm_subnet.project-subnet-vm1.id
+}
