3ce3acc470bb5c5394a2baa86a31cc874c9879b0
JackR42 <j.roumimper@gmail.com>
2023-07-14 16:13:06 +0200

    Update and rename vm1.tf to vm.tf

--- vm.tf
+++ vm.tf
@@ -35,3 +35,19 @@ resource "azurerm_windows_virtual_machine" "project-vm1" {
     version   = "latest"
   }
 }
+
+# Auto shutdown VM
+
+resource "azurerm_dev_test_global_vm_shutdown_schedule" "project-shutdown" {
+  virtual_machine_id = azurerm_windows_virtual_machine.project-vm1.id
+  resource_group_name = azurerm_resource_group.project.name
+  location            = azurerm_resource_group.project.location
+  enabled            = true
+
+  daily_recurrence_time = "2000"
+#  timezone              = "AUS Eastern Standard Time"
+
+  notification_settings {
+    enabled         = true   
+  }
+ }
