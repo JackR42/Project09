fa21d947daf3886410c1e5b06358c3c9348ec438
JackR42 <j.roumimper@gmail.com>
2023-07-14 13:30:41 +0200

    Update network-variables.tf

--- network-variables.tf
+++ network-variables.tf
@@ -2,3 +2,7 @@ variable "vnet-cidr" {
   type        = string
   description = "The CIDR of the VNET Virtual Network"
 }
+variable "subnet-cidr-vm1" {
+  type        = string
+  description = "The CIDR of the SUBNET to be assigned to the VNET for VMs"
+}
