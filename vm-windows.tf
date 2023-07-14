753bc8a420e077c6c341170dcb7e14bb17d71d67
JackR42 <j.roumimper@gmail.com>
2023-07-14 14:15:26 +0200

    Update vm-windows.tf

--- vm-windows.tf
+++ vm-windows.tf
@@ -11,8 +11,8 @@ resource "azurerm_network_interface" "project-nic-vm-win01" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "project-vm-win01" {
-  name                = "${var.app-name}-vm-win01-${var.env-name}"
+resource "azurerm_windows_virtual_machine" "project-vm1" {
+  name                = "VM1"
   resource_group_name = azurerm_resource_group.project.name
   location            = azurerm_resource_group.project.location
   size                = "Standard_DS2_v2"
