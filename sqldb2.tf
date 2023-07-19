31003f76cca8a7e42fbf790f67428758a9040d91
JackR42 <j.roumimper@gmail.com>
2023-07-19 14:03:06 +0000

    Adding FW for DB2

--- sqldb2.tf
+++ sqldb2.tf
@@ -17,3 +17,25 @@ resource "azurerm_mssql_server" "project-sqldb2" {
     Environment = var.env-name
   }
 }
+
+# Create FW rule to allow access from AZURE SERVICES, e.g. PowerApp
+resource "azurerm_mssql_firewall_rule" "project-sqldb2fw0" {
+  name = "FirewallRulesqldb2fw0"
+  server_id = azurerm_mssql_server.project-sqldb2.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address = "0.0.0.0"
+}
+# Create FW rule to allow access from OFFICE
+resource "azurerm_mssql_firewall_rule" "project-sqldb2fw1" {
+  name = "FirewallRuledsqlb2fw1"
+  server_id = azurerm_mssql_server.project-sqldb2.id
+  start_ip_address = "91.205.194.1"
+  end_ip_address = "91.205.194.1"
+}
+# Create FW rule to allow access from HOME
+resource "azurerm_mssql_firewall_rule" "project-sqldb2fw2" {
+  name = "FirewallRulesqldb2fw2"
+  server_id = azurerm_mssql_server.project-sqldb2.id
+  start_ip_address = "94.209.108.55"
+  end_ip_address = "94.209.108.55"
+}
