b5d383eca44c59d01c98810ba8d17a53c5265c48
JackR42 <j.roumimper@gmail.com>
2023-07-19 13:15:31 +0000

    Adding SQLDB2

--- sqldb2.tf
+++ sqldb2.tf
@@ -1,7 +1,8 @@
-resource "azurerm_mssql_server" "project-sqldb1" {
-  name  = data.azurerm_key_vault_secret.secret6.value
+resource "azurerm_mssql_server" "project-sqldb2" {
+  name  = data.azurerm_key_vault_secret.secret7.value
   resource_group_name = azurerm_resource_group.project.name
-  location            = azurerm_resource_group.project.location
+#  location            = azurerm_resource_group.project.location
+  location            = "East US"
   version = "12.0"
   administrator_login          = data.azurerm_key_vault_secret.secret2.value
   administrator_login_password = data.azurerm_key_vault_secret.secret3.value
