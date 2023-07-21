d97739a82d30fe4e5ceb49ba2bb020685c43a7c6
JackR42 <j.roumimper@gmail.com>
2023-07-21 08:42:21 +0000

    Renamed FW rule

--- scripts/VM-Configure.ps1
+++ scripts/VM-Configure.ps1
@@ -5,4 +5,4 @@ param
     [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$Project
 )
 
-New-NetFirewallRule -DisplayName '$Project - Allow SQL Server inbound' -Direction Inbound -Protocol TCP -LocalPort 1433 -Action Allow
+New-NetFirewallRule -DisplayName '$Project - Hello DBAs - Allow SQL Server inbound' -Direction Inbound -Protocol TCP -LocalPort 1433 -Action Allow
